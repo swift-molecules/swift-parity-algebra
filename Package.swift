85767e306294637c8791ee5e703a7079bc303f43
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:04 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-parity-algebra-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -22,9 +22,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-parity-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-algebra-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-optic-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-parity-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-algebra-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-optic-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -39,7 +48,7 @@ let package = Package(
         .target(
             name: "Parity Algebra Primitives Test Support",
             dependencies: [
-                "Parity Algebra Primitives",
+                "Parity Algebra Primitives"
             ],
             path: "Tests/Support"
         ),
